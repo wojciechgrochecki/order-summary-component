9119d5c45c7baef35ed4bdbf76548f2ddf5a5561
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-17 13:39:49 +0200

    center component & add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import OrderSummary from "./components/OrderSummary";
+import backgroundMobile from "./assets/images/pattern-background-mobile.svg";
 
 function App() {
   return (
-    <div className="">
+    <div
+      className="grid h-full place-items-center bg-primary-pale-blue 
+    bg-[url('./assets/images/pattern-background-mobile.svg')] bg-contain bg-no-repeat md:bg-[url('./assets/images/pattern-background-desktop.svg')]"
+    >
       <OrderSummary />
     </div>
   );
