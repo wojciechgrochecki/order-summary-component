ca78bdd3b29ff84f284d179c12a584da4bb8437b
Wojciech Grochecki <wojciech5522@gmail.com>
2023-07-19 16:41:07 +0200

    fix behaviour on small height screens & add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,21 @@ import backgroundMobile from "./assets/images/pattern-background-mobile.svg";
 function App() {
   return (
     <div
-      className="grid h-full place-items-center bg-primary-pale-blue 
-    bg-[url('./assets/images/pattern-background-mobile.svg')] bg-contain bg-no-repeat md:bg-[url('./assets/images/pattern-background-desktop.svg')]"
+      className="grid min-h-full place-items-center content-center bg-primary-pale-blue bg-[url('./assets/images/pattern-background-mobile.svg')] 
+    bg-contain bg-no-repeat align-middle md:bg-[url('./assets/images/pattern-background-desktop.svg')]"
     >
       <OrderSummary />
+      <div className="  w-[90%] text-center">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a className="   whitespace-nowrap text-primary-bright-blue" href="#">
+          Wojciech Grochecki
+        </a>
+        .
+      </div>
     </div>
   );
 }
